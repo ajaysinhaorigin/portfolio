0b3b6d9fbfd563dfbfcfea3c059a352c0a488217
ajaysinhaorigin <ajaysinhaa09@gmail.com>
2023-03-11 10:16:55 +0530

    did some changes

--- src/components/about/components/AboutText.tsx
+++ src/components/about/components/AboutText.tsx
@@ -7,7 +7,7 @@ function AboutText() {
         Web developer Proficient in ReactJs ,Javascript and CSS. It was all
         started from my college days when I bought my first laptop and entered
         in the world of web-development. Usually, I spend 7 to 9 hours a day
-        with my laptop as I am coding, making beautifull websites that are live
+        with my laptop as I am coding, making beautiful websites that are live
         on internet and continously improving my chops by learning new skills
         day by day.
       </p>
