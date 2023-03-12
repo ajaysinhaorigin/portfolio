ff86cf7dd7a32837c2d013fd14098daf141d254d
ajaysinhaorigin <ajaysinhaa09@gmail.com>
2023-03-12 16:36:16 +0530

    changed image

--- src/components/home/components/BigImage.tsx
+++ src/components/home/components/BigImage.tsx
@@ -2,7 +2,7 @@ import bigImg from '../../../assets/myImage/myBigImg.png'
 
 function BigImage() {
   return (
-    <div className="mt-4 justify-items-end hidden md:block h-[85%] rounde-full">
+    <div className="mt-4 justify-items-end hidden md:block h-[90%] rounde-full">
       <img src={bigImg} alt="" className="w-full h-full bg-contain" />
     </div>
   )
