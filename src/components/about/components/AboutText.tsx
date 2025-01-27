4c056ecc133e9dff934cf25b5119d36b9dc39b66
Ajay Sinha <ajaysinhaa09@gmail.com>
2025-01-27 23:29:10 +0530

    updated about me content

--- src/components/about/components/AboutText.tsx
+++ src/components/about/components/AboutText.tsx
@@ -4,9 +4,9 @@ function AboutText() {
     <div className="my-auto text-start">
       <Heading heading="About" alignment="text-start mb-4" />
       <p className="text-sm lg:text-base">
-        Web developer Proficient in ReactJs, Javascript and CSS. It was all
+        A Software developer Proficient in ReactJs, Next.js and React Native. It was all
         started from my college days when I bought my first laptop and entered
-        in the world of web-development. Usually, I spend 7 to 9 hours a day
+        in the world of software-development. Usually, I spend 7 to 9 hours a day
         with my laptop as I am coding, building beautiful web-applications that
         are live on internet and continously improving my chops by learning new
         skills day by day.
